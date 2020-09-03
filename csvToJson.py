da02f26b566db25fa2e44ab02308c0cda0e25eba
Eyob Abegaz <70550487+EyobAbegaz@users.noreply.github.com>
2020-09-03 01:51:27 -0400

    time.json file created, some more comments added

--- csvToJson.py
+++ csvToJson.py
@@ -1,27 +1,45 @@
+# ISTE 432
+# Eyob Abegaz, 
+# Exercise - 2
+# Converting CSV file to JESON file
+
+# Import csv module to read and write tabular data in CSV format
+# Import json module to convert the python dictionary into a json string 
+# that can be written into a file
+# Import time module to get today's date in YYYYMMDD format
 import csv
 import json
+import time
 
-with open('mockData.csv', 'r') as inp, open('my.csv', 'w') as out:
-    for line in inp:
+# Assign today's date in YYYYMMDD format to a variable 
+my_date = time.strftime('%Y%m%d')
+
+# Read and fix the original csv file and
+# write the fixed data on another csv file
+with open('mockData.csv', 'r') as inpu, open('fixed_mocked.csv', 'w') as out:
+    for line in inpu:
         l=line.replace(', Jr,', ',').replace(', III,', ',').replace(', ', ',').lower()
         out.write(l)
-# create a dictionary 
+
+# Create a dictionary 
 data = {} 
       
 # Open a csv reader called DictReader 
-with open('my.csv', 'r') as csvf: 
-     csvReader = csv.DictReader(csvf) 
+with open('fixed_mocked.csv', 'r') as f: 
+     csvReader = csv.DictReader(f) 
           
      # Convert each row into a dictionary  
      # and add it to data 
      for rows in csvReader: 
               
-         # Assuming a column named 'No' to 
+         # Assuming a column named 'id' to 
          # be the primary key 
          key = rows['id'] 
          data[key] = rows 
   
 # Open a json writer, and use the json.dumps()  
 # function to dump data 
-with open('my.json', 'w') as jsonf: 
-    jsonf.write(json.dumps(data, indent=4))
+with open(str(my_date) +'.json', 'w') as jf:
+    
+    # Make it more readable and pretty while writing
+    jf.write(json.dumps(data, indent=4))
